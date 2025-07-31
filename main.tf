c83db27a53786c022733c67c387960de313fcf38
Dennis Fridrich <fridrich.dennis@gmail.com>
2025-07-31 22:02:34 +0200

    Add SSH access

--- main.tf
+++ main.tf
@@ -7,7 +7,45 @@ resource "aws_instance" "homework-4" {
   instance_type = var.instance_type
   monitoring    = true
 
+  key_name = aws_key_pair.ssh_key.key_name
+  security_groups = [aws_security_group.ssh.name]
+
   tags = {
-    Name = "terraform-ec2-homework-4"
+    Name = "terraform-ec2-homework-4",
+    Name = "has-ssh-access"
   }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "ssh" {
+  name   = "terraform-ssh-sg"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-ssh-sg"
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,23 @@
 variable "aws_region" {
-  type        = string
-  default     = "eu-central-1"
+  type    = string
+  default = "eu-central-1"
 }
 
 variable "instance_type" {
-  type        = string
-  default     = "t3.micro" # Zmena oproti zadani kvuli regionu
+  type    = string
+  default = "t3.micro" # Zmena oproti zadani kvuli regionu
 }
 
 variable "ami_id" {
-  type        = string
+  type = string
+}
+
+variable "key_name" {
+  type    = string
+  default = "terraform-key"
+}
+
+variable "public_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
 }
